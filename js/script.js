503c3fa646a78bc3b83b38f2913210f1b847944a
P0cketbear <102195108+P0cketbear@users.noreply.github.com>
2024-01-18 01:31:15 -0300

    basic ops

--- js/script.js
+++ js/script.js
@@ -2,53 +2,83 @@
 
 // input vars
 
-let a, b;
+let a = "";
+let b = "";
 let ops = "";
 
 // operations functions
 
 function add(a, b) {
-    return a + b;
+
+    return parseFloat(a) + parseFloat(b);
 
 }
 function substract(a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);;
 }
 function multiply(a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 }
 function divide(a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);;
 }
 
 function operate(a, ops, b) {
 
     switch (ops) {
         case "+":
-            console.log(add(a, b))
+            result.textContent = add(a, b);
             break;
         case "-":
-            console.log(substract(a, b))
+            result.textContent = substract(a, b)
+            break;
+        case "x":
+            result.textContent = multiply(a, b)
             break;
         case "*":
-            console.log(multiply(a, b))
+            result.textContent = multiply(a, b)
             break;
         case "/":
-            console.log(divide(a, b))
+            result.textContent = divide(a, b).toFixed(1)
             break;
     }
 
 }
 
-
-const btn_number = document.querySelectorAll('.btn_number');
+// inputs buttons selectors
 const btn_options = document.querySelectorAll('.btn_options');
 const btn_operators = document.querySelectorAll('.btn_operators');
+const btn_number = document.querySelectorAll('.btn_number');
+const btn_equals = document.querySelector('.btn_equals')
 
 
-btn_number.forEach((el, i) => {
-    el.addEventListener('click', () => console.log(el.textContent))
+// Screen selectors
+const inputs = document.querySelector('.inputs');
+const result = document.querySelector('.result');
+
+
+btn_number.forEach((btnN) => {
+    btnN.addEventListener('click', () => {
+        if (ops === "") {
+            a += btnN.textContent;
+            inputs.textContent = a
+        } else {
+            b += btnN.textContent;
+            inputs.textContent = b
+        }
+
+    });
+});
+
+btn_operators.forEach((btnOp) => {
+    btnOp.addEventListener('click', () => {
+        ops = btnOp.textContent;
+        inputs.textContent += ops;
+    })
+
 })
-console.log(btn_number)
-console.log(btn_options)
-console.log(btn_operators)
+
+btn_equals.addEventListener('click', () => operate(a, ops, b))
+
+
+
